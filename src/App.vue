3c637646cf0e9cc9c5fdb4a5a0478e46863b0fba
nv7z2 <barankacper2000@gmail.com>
2021-12-29 02:13:40 +0100

    Meta tags fix

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,54 @@
   </div>
 </template>
 
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: 'Dephug | Bring your ideas to life',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Bring your ideas to life! With Dephug you can make your business more attractive to customers.',
+        },
+        {
+          property: 'og:title',
+          content: 'Dephug | Bring your ideas to life',
+        },
+        {
+          property: 'og:url',
+          content: 'https://dephug.com',
+        },
+        {
+          property: 'og:image',
+          content:
+            'https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          property: 'og:image:url',
+          content:
+            'https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          property: 'og:image:secure_url',
+          content:
+            'https://images.pexels.com/photos/3813324/pexels-photo-3813324.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          property: 'og:description”',
+          content:
+            'Bring your ideas to life! With Dephug you can make your business more attractive to customers.',
+        },
+        { property: 'og:site_name', content: 'Dephug' },
+        { property: 'og:type', content: 'website' },
+        { name: 'robots', content: 'index,follow' },
+      ],
+    };
+  },
+};
+</script>
+
 <style lang="scss">
 @import './style/init';
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './languages';
+import VueMeta from 'vue-meta';
 import './registerGlobalComponents';
 import './email';
 
 Vue.config.productionTip = true;
+Vue.use(VueMeta);
 
 new Vue({
   router,
